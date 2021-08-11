1fa9258edb79860442b7c73fb5aa0f550c96f887
johnnycistudent <42033483+johnnycistudent@users.noreply.github.com>
2021-08-11 23:34:47 +0100

    movie search fixed

--- OneDrive/Desktop/NCI/Project/reViewer/reViewer/src/main/java/com/reviewer/AppController.java
+++ OneDrive/Desktop/NCI/Project/reViewer/reViewer/src/main/java/com/reviewer/AppController.java
@@ -18,12 +18,14 @@ public class AppController {
 
     // home
     @GetMapping("/")
-    public String viewHomePage(){
+    public String viewHomePage(Model model){
+        model.addAttribute("pageTitle", "re:Viewer Homepage");
         return "index";
     }
 
     @GetMapping("/403")
-    public String error403() {
+    public String error403(Model model) {
+        model.addAttribute("pageTitle", "403 error");
         return "403";
     }
 
